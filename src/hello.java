af4b9d11b39fe9f9988fd87d57ceb003c8d8b531
1161325721 <1161325721@qq.com>
2020-02-18 10:05:05 +0800

    Initial commit

---
+++ src/hello.java
@@ -0,0 +1,19 @@
+/**
+ * @program: gittest
+ * @ClassName: hello
+ * @description:
+ * @author: JX190728
+ * @create: 2020-02-18 10:03
+ **/
+public class hello
+{
+
+
+	public static void main(String[] args)
+	{
+
+	}
+
+
+
+}
